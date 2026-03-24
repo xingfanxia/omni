cf71d367f62ce7ee4e301431fae3f3c761cd8f8d
Praveen Sampath <praveensampath2508@gmail.com>
2026-03-24 11:01:39 +0530

    Remove PDF extraction endpoint from AI service

--- services/ai/processing/__init__.py
+++ services/ai/processing/__init__.py
@@ -1,13 +1,5 @@
 from .chunking import Chunker
-from .pdf import (
-    PDFExtractionRequest,
-    PDFExtractionResponse,
-    extract_text_from_pdf,
-)
 
 __all__ = [
     "Chunker",
-    "PDFExtractionRequest",
-    "PDFExtractionResponse",
-    "extract_text_from_pdf",
 ]
